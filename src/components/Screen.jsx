f2ed90c9be28a5f0741396edada0cc3f78e0a87c
Eliezer Nsengi <avaranstudios@gmail.com>
2024-07-31 11:50:38 +0200

    feat: screen component

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,7 +1,18 @@
 import PropTypes from "prop-types";
 
 const Screen = (props) => {
-  return <div className="text-white bg-gray-500 py-5">screen</div>;
+  return (
+    <div className="text-white ">
+      <input
+        type="text"
+        name="screen"
+        className="w-full h-full bg-gray-500 py-2 px-2 outline-none text-4xl"
+        readOnly
+        dir="rtl"
+        value={0}
+      />
+    </div>
+  );
 };
 
 Screen.propTypes = {};
